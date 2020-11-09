9b1ae2a80774ad6fb72c69a2e0e0fbee043f573e
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-11-09 23:58:33 +0100

    add(lab06): added 6th laboratory with CASE and CTE queries

---
+++ Lab 6 - CASE, CTE/Lab-6.sql
@@ -0,0 +1,43 @@
+-- 1. Lista z nazwiskami i imionami uczestników z kolumną o wartościach ('duża', 'średnia', 'mała', 'brak'). Wartości tej kolumny zależne od kolumny obecnosc.
+
+WITH obecnosci as ( 
+	SELECT SUM(fk.obecnosc) as obec, u.id_uczestnik
+    FROM uczestnik u JOIN uczest_kurs uk ON u.id_uczestnik=uk.id_uczestnik
+	JOIN frekwencja fk ON fk.id_uczestnik = uk.id_uczestnik
+    WHERE fk.id_uczestnik = uk.id_uczestnik
+    GROUP BY u.id_uczestnik
+) SELECT DISTINCT u.imie, u.nazwisko,
+  CASE 
+     WHEN ob.obec >= 5 and ob.obec < 9 THEN 'mała'
+     WHEN ob.obec >= 9 and ob.obec < 11 THEN 'średnia'
+     WHEN ob.obec >= 11 THEN 'duża'
+     ELSE 'brak'
+  END
+FROM obecnosci ob 
+JOIN uczestnik u ON ob.id_uczestnik = u.id_uczestnik;
+
+
+-- 2. Lista z nazwiskami i imionami wykładowców z kolumną o wartościach ('poniżej limitu', 'limit', 'powyżej limitu' ). Wartości tej kolumny zależne od ilosci uczestników na kursach danego wykładowcy.
+
+SELECT wyk.imie, wyk.nazwisko, 
+
+  CASE 
+      WHEN count(id_uczestnik) < 13 THEN 'poniżej limitu'
+      WHEN count(id_uczestnik) = 13 THEN 'limit'
+      WHEN count(id_uczestnik) > 13 THEN 'powyżej limitu'
+      ELSE 'brak'
+  END
+  
+FROM wykladowca wyk
+JOIN wykl_kurs wy_ku ON wy_ku.id_wykl = wyk.id_wykladowca
+JOIN uczest_kurs ucz_kur on wy_ku.id_kurs = ucz_kur.id_kurs
+GROUP BY imie, nazwisko;
+
+
+-- 3. Korzystając z CTE pokazać średnią ilość uczestników na kursie. Nie korzystamy z agregatu AVG.
+
+WITH statementCTE AS ( select id_kurs, id_grupa, count(*) as num from uczest_kurs
+group by id_kurs, id_grupa )
+SELECT sum(num) / count(ko.opis) as srednia_ilosc_uczestnikow_na_kursie
+ from statementCTE JOIN kurs k USING ( id_kurs, id_grupa)
+ JOIN kurs_opis ko ON k.id_kurs_nazwa = ko.id_kurs
